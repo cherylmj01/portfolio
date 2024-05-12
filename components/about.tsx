b7060d8c4c5e02d7e9c48c0074ed7bcbc2e7aad7
cherylmj01 <cherylmj01@gmail.com>
2024-05-11 18:13:35 -0600

    Latest portfolio code

---
+++ components/about.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from 'react';
+import SectionHeading from './section-heading';
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section 
+    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    initial={{ opacity : 0, y : 100 }}
+    animate = {{ opacity : 1, y : 0 }}
+    transition={{ delay: 0.175 }}
+    >
+        <SectionHeading>About Me</SectionHeading>
+        <p className="mb-3">
+        With a foundation in <span className="font-bold">Computer Science</span> 
+        and a rich five-year tenure as a Solution Architect, 
+        I’ve turned my passion for technology into a career focused on building innovative solutions. 
+        I specialize in <span className="font-bold">Python, JavaScript, and SQL</span>, 
+        bringing ideas to life through code.
+        </p>
+
+        <p className="mb-3">
+            <span className="font-underline">I am driven</span> by the thrill of creating and enhancing functionalities, 
+            constantly seeking to bridge the gap between data analysis and actionable insights. 
+            My current focus lies in the dynamic fields of 
+            <span className="font-bold"> Machine Learning and Artificial Intelligence</span>.
+        </p>
+
+        <p className="mb-3">
+            <span className="italic">Beyond the screen</span>, 
+            I’m a globe-trotter, culinary explorer, and a fan of hands-on crafts like crochet and painting. 
+            These diverse interests fuel my creativity and broaden my perspective, enriching my professional and personal life.
+        </p>
+
+        <p className="mb-3">
+            Whether coding or exploring, I am always on a quest to learn and grow, 
+            embracing new challenges and opportunities to innovate.
+        </p>
+
+    </motion.section>
+  )
+}
