1c756455ddafd4e1a3f1dd73100049a77d4ea311
cherylmj01 <cherylmj01@gmail.com>
2024-06-03 12:53:59 -0600

    Latest portfolio code

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,9 @@ export default function Intro() {
       }}
       >
         <a 
-        className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-black/10 dark:bg-white/10"
+        className="group bg-white px-7 py-3 flex items-center 
+        gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 
+        active:scale-105 transition cursor-pointer border-black/10 dark:bg-white/10"
         href="/CV.pdf" 
         download
         >
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -6,7 +6,7 @@ import type { SectionName } from './types';
 export function useSectionInView(sectionName :SectionName, threshold = 0.75) 
 {
     const { ref, inView } = useInView({
-        threshold,
+        threshold: 0.5,
       });
       const {setActiveSection, timeOfLastClick} = useActiveSectionContext();
     
@@ -16,5 +16,5 @@ export function useSectionInView(sectionName :SectionName, threshold = 0.75)
         }
       }, [inView, setActiveSection, timeOfLastClick, sectionName])
       
-      return {ref,};
+      return {ref, inView};
 }
